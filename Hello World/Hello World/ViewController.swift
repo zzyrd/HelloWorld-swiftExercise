7dc01f31d6b023d14c3fb0faa10b5082f50bf95a
zzyrd <zhangzhihao@mwc-065140.dhcp.missouri.edu>
2017-04-06 18:58:06 -0500

    add new branch and add say functionality

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Hello wORLD")
+        say("Hello World")
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func say(_ text: String) {
+        print("Hello World")
+        
     }
 
 
